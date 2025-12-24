71a2605e7630ec7ffe25c122a66945046f8d40e9
aritra-tech <aritrarick2002@gmail.com>
2025-12-24 12:25:51 +0530

    feat: update Ktor client configuration and switch to Bearer token authentication

--- app/src/main/java/com/aritradas/movieapp/di/AppModule.kt
+++ app/src/main/java/com/aritradas/movieapp/di/AppModule.kt
@@ -1,5 +1,7 @@
 package com.aritradas.movieapp.di
 
+import android.R.attr.level
+import android.util.Log
 import com.aritradas.movieapp.BuildConfig
 import com.aritradas.movieapp.data.remote.ApiServices
 import com.aritradas.movieapp.data.repository.MovieRepositoryImpl
@@ -7,7 +9,13 @@ import com.aritradas.movieapp.domain.repository.MovieRepository
 import com.aritradas.movieapp.presentation.movies.MoviesViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -15,30 +23,56 @@ import org.koin.dsl.module
 
 val appModule = module {
     single {
+        val bearerToken = BuildConfig.TMDB_BEARER_TOKEN
+
         HttpClient(Android) {
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.d("KtorClient", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+
             install(ContentNegotiation) {
                 json(
                     Json {
                         ignoreUnknownKeys = true
                         isLenient = true
+                        prettyPrint = true
                     }
                 )
+            }
+
+            install(HttpTimeout) {
+                requestTimeoutMillis = 30000
+                connectTimeoutMillis = 30000
+                socketTimeoutMillis = 30000
+            }
+
+            defaultRequest {
+                header("Authorization", "Bearer $bearerToken")
+                header("Accept", "application/json")
+            }
+
+            engine {
+                connectTimeout = 30_000
+                socketTimeout = 30_000
+                proxy = null
             }
         }
     }
 
     single {
-        ApiServices(
-            client = get(),
-            apiKeyProvider = {
-                BuildConfig.TMDB_API_KEY
-            }
-        )
+        ApiServices(client = get())
     }
 
-    single<MovieRepository> { MovieRepositoryImpl(apiServices = get()) }
+    single<MovieRepository> {
+        MovieRepositoryImpl(apiServices = get())
+    }
 
-    viewModel { MoviesViewModel(repository = get()) }
+    viewModel {
+        MoviesViewModel(repository = get())
+    }
 }
-
-
