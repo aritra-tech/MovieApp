22f218a7b2cb72b1cf4813b72c0886b079c094d0
aritra-tech <aritrarick2002@gmail.com>
2025-12-24 19:41:31 +0530

    feat: implement shared element transitions and update detail route arguments

--- app/src/main/java/com/aritradas/movieapp/ui/navigation/Navigation.kt
+++ app/src/main/java/com/aritradas/movieapp/ui/navigation/Navigation.kt
@@ -1,16 +1,18 @@
 package com.aritradas.movieapp.ui.navigation
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.aritradas.movieapp.presentation.movies.MoviesListScreens
 import com.aritradas.movieapp.presentation.favourites.FavouritesScreen
-import com.aritradas.movieapp.presentation.movies.MoviesViewModel
-import com.aritradas.movieapp.presentation.movieDetails.MovieDetailScreen
 import com.aritradas.movieapp.presentation.favourites.FavouritesViewModel
+import com.aritradas.movieapp.presentation.movieDetails.MovieDetailScreen
+import com.aritradas.movieapp.presentation.movies.MoviesListScreens
+import com.aritradas.movieapp.presentation.movies.MoviesViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -21,43 +23,57 @@ fun Navigation(
     val vm = viewModel ?: koinViewModel<MoviesViewModel>()
     val favouritesViewModel: FavouritesViewModel = koinViewModel()
 
-    NavHost(
-        navController = navController,
-        startDestination = Screens.Movies.route
-    ) {
-        composable(Screens.Movies.route) {
-            MoviesListScreens(
-                viewModel = vm,
-                onMovieClick = { movieId ->
-                    navController.navigate(Screens.Detail.create(movieId))
-                },
-                onFavouritesClick = {
-                    navController.navigate(Screens.Favourites.route)
-                }
-            )
-        }
-        composable(Screens.Favourites.route) {
-            FavouritesScreen(
-                viewModel = favouritesViewModel,
-                onBack = { navController.popBackStack() },
-                onMovieClick = { movieId ->
-                    navController.navigate(Screens.Detail.create(movieId))
-                }
-            )
-        }
-        composable(
-            route = Screens.Detail.route,
-            arguments = listOf(navArgument("movieId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val movieId = backStackEntry.arguments?.getInt("movieId") ?: return@composable
-            
-            MovieDetailScreen(
-                movieId = movieId,
-                onBack = { navController.popBackStack() },
-                onFavoriteToggle = {
-                    vm.toggleFavorite(movieId)
-                }
-            )
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    SharedTransitionLayout {
+        NavHost(
+            navController = navController,
+            startDestination = Screens.Movies.route
+        ) {
+            composable(Screens.Movies.route) {
+                MoviesListScreens(
+                    viewModel = vm,
+                    onMovieClick = { movieId, posterPath ->
+                        navController.navigate(Screens.Detail.create(movieId, posterPath))
+                    },
+                    onFavouritesClick = {
+                        navController.navigate(Screens.Favourites.route)
+                    },
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    animatedContentScope = this@composable
+                )
+            }
+            composable(Screens.Favourites.route) {
+                FavouritesScreen(
+                    viewModel = favouritesViewModel,
+                    onBack = { navController.popBackStack() },
+                    onMovieClick = { movieId, posterPath ->
+                        navController.navigate(Screens.Detail.create(movieId, posterPath))
+                    },
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    animatedContentScope = this@composable
+                )
+            }
+            composable(
+                route = Screens.Detail.route,
+                arguments = listOf(
+                    navArgument("movieId") { type = NavType.IntType },
+                    navArgument("posterPath") { type = NavType.StringType }
+                )
+            ) { backStackEntry ->
+                val movieId = backStackEntry.arguments?.getInt("movieId") ?: return@composable
+                val posterPath = backStackEntry.arguments?.getString("posterPath") ?: ""
+
+                MovieDetailScreen(
+                    movieId = movieId,
+                    initialPosterPath = posterPath,
+                    onBack = { navController.popBackStack() },
+                    onFavoriteToggle = {
+                        vm.toggleFavorite(movieId)
+                    },
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    animatedContentScope = this@composable
+                )
+            }
         }
     }
 }
